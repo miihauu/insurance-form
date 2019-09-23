a584779eaeffabc808fa9663003c80eb4d32ac61
unknown <miihauu@gmail.com>
2019-09-23 22:16:37 +0200

    Updates

---
+++ precache-manifest.d3d044222af7331361798df025770c4c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d32c6d574abe8b0b01ede7d168df9b",
+    "url": "/miihauu/insurance-form/index.html"
+  },
+  {
+    "revision": "eedcd313b07d1bb55920",
+    "url": "/miihauu/insurance-form/static/js/2.2591df6b.chunk.js"
+  },
+  {
+    "revision": "89eb3334041aabe2d83c",
+    "url": "/miihauu/insurance-form/static/js/main.76b45605.chunk.js"
+  },
+  {
+    "revision": "fa13379f134033e93972",
+    "url": "/miihauu/insurance-form/static/js/runtime-main.2a77953d.js"
+  }
+]);
